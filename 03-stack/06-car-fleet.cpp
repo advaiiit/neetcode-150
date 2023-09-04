689870f6dba474432f6dc73b025093dd06c5c2a1
Advait Kamathe <advaitkamathe@gmail.com>
2023-09-04 23:19:09 +0530

    Adding 06-car-fleet.cpp

---
+++ 03-stack/06-car-fleet.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int numCars = position.size();
+        vector<pair<int, int>> positionAndSpeed;
+        stack<float> stack;
+
+        for (int i = 0; i < numCars; i++) 
+            positionAndSpeed.push_back({position[i], speed[i]});
+
+        sort(positionAndSpeed.begin(), positionAndSpeed.end());
+
+        for (int i = 0; i < numCars; i++) {
+            auto [position, speed] = positionAndSpeed[i];
+            float time = (float) (target - position) / speed;
+            
+            while (!stack.empty() && time >= stack.top()) stack.pop();
+
+            stack.push(time);
+        }
+
+        return stack.size();
+    }
+};
+// TC: O(N) SC: O(N)
