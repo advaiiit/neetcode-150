858d66068a6e0a6540d25acdf29654a2d0fcc0f2
Advait Kamathe <advaitkamathe@gmail.com>
2023-09-06 00:09:43 +0530

    Adding 07-largest-rectangle-in-historgram.cpp

---
+++ 03-stack/07-largest-rectangle-in-historgram.cpp
@@ -0,0 +1,52 @@
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    int brute(vector<int>& heights) {
+        int maxArea = 0, totalBars = heights.size();
+
+        for (int i = 0; i < totalBars; i++) {
+            int minHeight = heights[i];
+
+            for (int j = i; j < totalBars; j++) {
+                minHeight = min(minHeight, heights[j]);
+                maxArea = max(maxArea, minHeight * (j - i + 1));
+            }
+        }
+
+        return maxArea;
+    }
+    // TC: O(N^2) SC:(1)
+
+    int multiPass(vector<int>& heights) {
+        int maxArea = 0, totalBars = heights.size();
+        vector<int> prevSmall(totalBars), nextSmall(totalBars);
+        stack<int> stack;
+
+        for (int i = 0; i < totalBars; i++) {
+            while (!stack.empty() && heights[stack.top()] >= heights[i]) 
+                stack.pop();
+
+            prevSmall[i] = (stack.empty()) ? 0 : stack.top() + 1;
+            stack.push(i);
+        }
+
+        while (!stack.empty()) stack.pop();
+
+        for (int i = totalBars - 1; i >= 0; i--) {
+            while (!stack.empty() && heights[stack.top()] >= heights[i]) 
+                stack.pop();
+            
+            nextSmall[i] = (stack.empty()) ? totalBars - 1 : stack.top() - 1;
+            stack.push(i);
+        }
+
+        for (int i = 0; i < totalBars; i++) 
+            maxArea = max(maxArea, (heights[i] * (nextSmall[i] - prevSmall[i] + 1)));
+
+        return maxArea;
+    }
+    // TC: O(N) SC: O(N)
+};
