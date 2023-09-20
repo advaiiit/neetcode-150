b01242bc1bc9e146744aed75604b92e504043bc7
Advait Kamathe <advaitkamathe@gmail.com>
2023-09-20 22:31:51 +0530

    Adding 02-merge-two-sorted-list.cpp

---
+++ 06-linked-list/02-merge-two-sorted-list.cpp
@@ -0,0 +1,60 @@
+#include<algorithm>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* usingExtraSpace(ListNode* list1, ListNode* list2) {
+        ListNode* head = new ListNode();
+        ListNode* ptr = head;
+
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                ptr->next = list1;
+                list1 = list1->next;
+            } else {
+                ptr->next = list2;
+                list2 = list2->next;
+            }
+            
+            ptr = ptr->next;
+        }
+
+        ptr->next = (list1) ? list1 : list2;
+
+        return head->next;
+    }
+    // TC: O(N) SC: O(N)
+    // N: total length of both lists
+
+    ListNode* withoutExtraSpace(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL) return list2;
+        if (list2 == NULL) return list1;
+
+        if (list1->val > list2->val) swap(list1, list2);
+        ListNode* head = list1;
+
+        while (list1 && list2) {
+            ListNode* temp = new ListNode;
+            
+            while (list1 && list1->val <= list2->val) {
+                temp = list1;
+                list1 = list1->next;
+            }
+
+            temp->next = list2;
+            swap(list1, list2);
+        }
+
+        return head;
+    }
+    // TC: O(N) SC: O(1)
+    // N: total length of both lists
+};
